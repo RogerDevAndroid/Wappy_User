424ddaeb56fb4057c0161c374069fd55a662a400
Jose Rogelio Garcia Vital <rgarcia@softvibes.com.mx>
2024-09-26 16:49:17 -0500

    Remove Comments

--- lib/app/backend/models/slot_time_model.dart
+++ lib/app/backend/models/slot_time_model.dart
@@ -1,11 +1,4 @@
-/*
-  Authors : initappz (Rahul Jograna)
-  Website : https://initappz.com/
-  App Name : Ultimate Salon Full App Flutter V3
-  This App Template Source code is licensed as per the
-  terms found in the Website https://initappz.com/license
-  Copyright and Good Faith Purchasers © 2024-present initappz.
-*/
+
 class SlotTimeModel {
   String? startTime;
   String? endTime;
--- lib/app/view/firebase.dart
+++ lib/app/view/firebase.dart
@@ -1,11 +1,4 @@
-/*
-  Authors : initappz (Rahul Jograna)
-  Website : https://initappz.com/
-  App Name : Ultimate Salon Full App Flutter V3
-  This App Template Source code is licensed as per the
-  terms found in the Website https://initappz.com/license
-  Copyright and Good Faith Purchasers © 2024-present initappz.
-*/
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:user/app/controller/firebase_controller.dart';
